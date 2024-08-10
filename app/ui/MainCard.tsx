d8b25888a356406b554418aca734ad4533e35e61
freakssha <lestreng.begi@gmail.com>
2024-08-10 21:22:29 +0300

    ты сам отдайся мне. необратимый враг, — Сам — мне

---
+++ app/ui/MainCard.tsx
@@ -0,0 +1,26 @@
+import React, { useRef, useState, useEffect, useLayoutEffect } from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../styles.module.css';
+
+console.clear();
+
+interface FlippableProps {
+    className?: string;
+    component?: React.ElementType<any>;
+    flipId: string;
+}
+
+const FLIP_TRANSITION_EVENT = "flip-transition";
+const FLIP_TRANSITION_END_EVENT = "flip-transition-end";
+
+export default function MainCard(callback: () => void, flipId: string) {
+ return (
+     <div className={styles.container}>
+
+         <div className={styles.s}></div>
+         <div className={styles.s}></div>
+     </div>
+ )
+}
+
+
